82876ff804ef8dd83f66b4a8af5fa0cbcd43e788
James Y <abcde201220122012@outlook.com>
2017-02-08 23:51:41 -0600

    Update dec2frac
    change the encoding line: previously mistakenly put #! instead of #
    remove re.fullmatch and replace its use by re.match because of weird travis-ci incompatibilities
    use the original name "_isZero" instead of "isZero" for consistency between dec2frac and other files
    correct the regular expression
    further working on the tech part but not fully finished

--- dec2frac.py
+++ dec2frac.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-#! -*- encoding: utf8 -*-
+# -*- encoding: utf8 -*-
 from fraction import frac
-from fraction import _isZero as isZero
-from re import match, fullmatch
+from fraction import _isZero
+from re import match
 
 def dec2frac(strDec):
     'input str, return frac'
     # Check if is str, and matches the re
     if type(strDec) is not str: raise TypeError
-    expression = '^(?={2,}\d*\.\d+$'
-    if not fullmatch(expression, strDec): raise ValueError
+    expression = '^(?=.{2,})\d*\.\d+$' # matches strict decimals (12.3, not 12. or 12; .3 works as well)
+    if not match(expression, strDec): raise ValueError
     # Seperate to list
     lstDec = strDec.split('.')
-    if [i for i in lstDec[1] if i != '0']: return frac.input(lstDec[0], 1)
-    
+    # case of 1.0, 2.000
+    if not [i for i in lstDec[1] if i != '0']: return frac().input(lstDec[0], 1)
+    # case of .2, .345
+    pass
+    # case of 1.234, 4.53
+    pass
