971f881eec1c68be3f6edbe93292287348e80176
James Y <abcde201220122012@outlook.com>
2017-02-10 22:30:13 -0600

    File Placeholder
    Added file placeholder for Command Line Interface of MI.

---
+++ cli.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- encoding: utf8 -*-
+'''
+file reserved for the Command Line Interface of MI,
+not implemented until having finished other parts
+'''
+
+class cli:
+    def __init__(self):
+        pass
+    
+pass
