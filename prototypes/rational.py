e1f5e8c487cf9a886458ed732d2509baaacb979d
James Y <abcde201220122012@outlook.com>
2017-03-10 19:31:42 -0600

    Added prototypes

---
+++ prototypes/rational.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+# -*- encoding: utf8 -*-
+
---
+++ prototypes/real.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- encoding: utf8 -*-
+
+from scalar import *
+
+class real(scalar):
+    'This is a prototype class for real numbers'
+    pass
